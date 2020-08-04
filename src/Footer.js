229be292b15907f977a216b2abe2b7d6628c0152
eriksanchez <erikcsanchezdev@gmail.com>
2020-08-04 15:08:49 -0500

    Footer styling added...

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,49 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from "@material-ui/core";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer_left"></div>
-      <div className="footer_center"></div>
-      <div className="footer_right"></div>
+      <div className="footer_left">
+        <img
+          className="footer_albumLogo"
+          src="https://www.udiscovermusic.com/wp-content/uploads/2017/11/Kanye-West-My-Beautiful-Dark-Twisted-Fantasy-album-cover-web-optimisd-820.jpg"
+          alt=""
+        />
+        <div className="footer_songInfo">
+          <h4>No song is playing</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
